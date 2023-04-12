78b704d0d472cf333127612bf814e629f2d34dfd
Fady El Gawly <fgawly@gmail.com>
2023-04-12 03:46:44 +0200

    first commit

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  return (
+    <Page narrowWidth>
+      <TitleBar title="Lokal Partner App" primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Heading>Welcome to Lokal Automated Partners App</Heading>
+                  <p>
+                    Ready to go? Start populating your app with some sample
+                    products to view and test in your store.{" "}
+                  </p>
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImage}
+                    alt="Nice work on building a Shopify app"
+                    width={120}
+                  />
+                </div>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
